0059fbe64356a9933bb27746c2fba00865784b84
Suzanne <suzanne.pons68@gmail.com>
2025-09-18 14:40:01 +0200

    Ajout des commentaires sur le fichier js de l'exercice 3

--- exercice_3/main.js
+++ exercice_3/main.js
@@ -1,7 +1,7 @@
-
+//On crée la variable carres qui va parcourir le DOM pour trouver tous les éléments contenant la classe carre
 const carres = document.querySelectorAll('.carre');
 
-
+//On crée une fonction de boucle qui va permettre que l'écouteur d'évenement va ajouter une class "active" au carré sur lequel on vient de cliquer pour qu'ensuite il puisse prendre les propriétés du css qui lui sont attribué 
 carres.forEach(carres => {
     carres.addEventListener('click', () => {
         carres.classList.toggle('active');
